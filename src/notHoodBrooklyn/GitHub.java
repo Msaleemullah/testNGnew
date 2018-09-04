95dfcad8e000d21f5352b886a5eb8d5318654f99
[Msaleemullah] <[sarimwali689@gmail.com]>
2018-09-03 20:40:50 -0400

    new practice

---
+++ src/notHoodBrooklyn/GitHub.java
@@ -0,0 +1,13 @@
+package notHoodBrooklyn;
+
+public class GitHub {
+   String st1 = "mercelin";
+   String st2 = "Sameer";
+   String st3 = "Saleem";
+
+   public void newGroup() {
+       System.out.println("name of students");
+
+   }
+}
+
---
+++ src/notHoodBrooklyn/TestGutHub.java
@@ -0,0 +1,11 @@
+package notHoodBrooklyn;
+
+public class TestGutHub {
+    public static void main(String[] args) {
+
+    }
+
+    GitHub students = new GitHub();
+
+}
+
